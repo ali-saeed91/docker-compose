a0c5b9670023f74c3c6ceab0f6a08d54fe120203
unknown <engineer.ali91@live.com>
2022-08-06 04:15:35 +0500

    Ali added Project files

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.11.0b4-alpine3.16
+
+# Any working directory can be chosen as per choice like '/' or '/home' etc
+# i have chosen /usr/app/src
+ADD app.py .
+# Now the structure looks like this '/usr/app/src/test.py'
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+#CMD instruction should be used to run the software
+#contained by your image, along with any arguments.
+EXPOSE 5000
+CMD [ "python", "./app.py"]
---
+++ app.py
@@ -0,0 +1,20 @@
+# REST APIs
+#Request Method:
+    # GET           Endpoint: www.godev.today/api/v1/testApi, It returns message 'Simple API Test: This App is working'
+    # GET           Endpoint: www.godev.today/api/v1/student, It returns list of all the students (JSON)
+    # POST
+    # PATCH
+    # DELETE
+
+# localhost = 127.0.0.1
+# Port number: 5000
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/api/v1/testApi')
+def testApp():
+    return jsonify({'message':'Simple API Test: This App is working'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
